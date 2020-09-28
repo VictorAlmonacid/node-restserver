622a77e47c2e8edcaace3f99d3828835157d948e
Victor <victooor2210@gmail.com>
2020-09-28 17:33:27 -0300

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,4 +4,29 @@
 // PUERTO
 //=======================
 
+const { mongo } = require("mongoose");
+
 process.env.PORT = process.env.PORT || 3000;
+
+//========================
+// ENTORNO
+//=======================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//========================
+// BASE DE DATOS
+//=======================
+
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+urlDB = 'mongodb://mongodb+srv://Zaratras22:<Vito2355>@cluster0.koiex.mongodb.net/cafe';
+}
+ process.env.NODE_ENV = urlDB;
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 require('./config/config');
 
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -10,36 +14,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json()) 
 
+app.use(require('./routes/usuario'));
 
-app.get('/Usuario', function (req, res) {
-  res.json('get Usuario')
+ 
+
+mongoose.connect(process.env.NODE_ENV,
+{useNewUrlParser: true, useCreateIndex: true},
+ (err, res) =>{
+if(err) throw err;
+console.log('Base de datos ONLINE');
 });
 
-app.post('/Usuario', function (req, res) {
-    let body = req.body;
-    if(body.nombre === undefined){
-    res.status(400).json({
-        ok: false,
-        mensaje: 'El nombre es necesario'
-    });
-    }else{
-        res.json({
-            body
-        })
-    }
-  });
-
-  app.put('/Usuario/:id', function (req, res) {
-      let id = req.params.id;
-    res.json({
-        id
-    });
-  });
-
-  app.delete('/Usuario', function (req, res) {
-    res.json('delete Usuario')
-  });
- 
 app.listen(process.env.PORT, ()=>{
 console.log('Escuchando puerto: ',3000);
 });
+
