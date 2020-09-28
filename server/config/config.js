e0af939e4e6580f60335f24df573b6020ab33bdb
Victor <victooor2210@gmail.com>
2020-09-28 20:06:52 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -24,7 +24,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-urlDB = 'mongodb+srv://Zaratras22:Vito2355@cluster0.koiex.mongodb.net/cafe';
+urlDB = process.env.MONGO_URI;
 }
  process.env.URLDB = urlDB;
 
