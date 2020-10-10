2feec7e19cd2ef6eee6b52e0e03d75f1ad312633
Victor <victooor2210@gmail.com>
2020-10-10 17:45:17 -0300

    Google sign in

--- server/config/config.js
+++ server/config/config.js
@@ -45,4 +45,7 @@ urlDB = process.env.MONGO_URI;
  process.env.URLDB = urlDB;
 
 
-
+//========================
+// Google Client ID
+//=======================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '1004710961864-bk909phfr2h0ad71dqceu9mg8iqeh7ta.apps.googleusercontent.com';
