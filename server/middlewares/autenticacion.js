f4fa0ef000fbb5700e9e95476c5a102519d10819
Victor <victooor2210@gmail.com>
2020-11-08 18:15:58 -0300

    Subida de archivos

--- server/middlewares/autenticacion.js
+++ server/middlewares/autenticacion.js
@@ -44,7 +44,30 @@ let verificarAdmin_role = (req, res, next)=>{
   
 }
 
+//============
+//verificar token para imagen
+//============
+let verificaTokenImg = (req, res, next)=>{
+
+  let token = req.query.token;
+  
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        err:{
+          message: 'Token no valido'
+        }
+      });
+    }
+
+    req.usuario = decoded.usuario;
+    next();
+  });
+}
+
 module.exports = {
   verificarToken,
-  verificarAdmin_role
+  verificarAdmin_role,
+  verificaTokenImg
 };
